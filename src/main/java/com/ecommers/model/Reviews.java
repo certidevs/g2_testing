d1f38ba72627faf35f5b07cd492fcaf03a1cd825
carloslopezalbarracin <carloslopezalbarracin@gmail.com>
2026-04-23 22:42:41 +0200

    feat: Testing push

--- src/main/java/com/ecommers/model/Reviews.java
+++ src/main/java/com/ecommers/model/Reviews.java
@@ -1,4 +1,21 @@
 package com.ecommers.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@Entity
+@AllArgsConstructor
+@ToString
 public class Reviews {
+
+    @Id
+    private UUID id;
 }
