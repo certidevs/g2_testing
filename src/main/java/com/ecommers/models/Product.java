66133f4f2f2570df71fd755120fbcd2eeaca423e
jeanpool <jeanpool_0612@hotmail.com>
2026-04-24 19:35:49 +0200

    comint de variables de Product.java (Actualizo) Agrego en el pom.xml una dependecia (jakarta-validation) porque agregue el @NotBlank para no dejar en blaco la casilla de nombre en producto

--- src/main/java/com/ecommers/models/Product.java
+++ src/main/java/com/ecommers/models/Product.java
@@ -1,7 +1,9 @@
 package com.ecommers.models;
-
-
+import jakarta.validation.constraints.NotBlank;
+// importado de jakarta.validation para validar que el
+// título del producto no esté en blanco
 import jakarta.persistence.*;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.*;
 
 @Getter
@@ -17,17 +19,26 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY) //Delegar el ID a la Base de Datos.
     private Long id;
 
-    @Column(unique = true) //El titulos del producto sea tipo Unico
-    String  Title;
+    @NotBlank //El titulos del producto Obligatorio y no puede estar en blanco
+    @Column(nullable = false)//Obliga a poner el Título porque prohìbe guardar danos nulos (vacios)
+    private String  title;
 
-    Double Price; //Precio del producto
+    @PositiveOrZero //El precio del producto no puede ser negativo
+    private Double price; //Precio del producto
 
-    String shortDescription; //Descripción corta del producto
+    @Builder.Default //por defecto True
+    private boolean available = true; //Indica si el producto está disponible para la venta.
 
-    String longDescription; //Descripción larga del producto
+    private String isbn ; // Codigo de barras para cada producto
 
-    Integer Stock; // Cantidad de productos disponibles en el inventario
+    @Column(length = 500) //Limitar la longitud de la descripción corta a 500 caracteres
+    private String shortDescription; //Descripción corta del producto
 
-    String imageUrl; //URL de la imagen del producto
+    @Column(length = 2000) //Limitar la longitud de la descripción larga a 2000 caracteres
+    private String longDescription; //Descripción larga del producto
+
+    private Integer stock; // Cantidad de productos disponibles en el inventario
+
+    private String imageUrl; //URL de la imagen del producto
 
 }
