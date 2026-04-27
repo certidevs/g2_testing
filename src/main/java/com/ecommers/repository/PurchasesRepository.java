df2a5e9aeeb4659d34fe9b09097ca86fff78afab
Alba Carrasco <albacarrasmel@gmail.com>
2026-04-27 15:51:32 +0200

    Creation of methods PurchasesRepository

--- src/main/java/com/ecommers/repository/PurchasesRepository.java
+++ src/main/java/com/ecommers/repository/PurchasesRepository.java
@@ -3,6 +3,12 @@ package com.ecommers.repository;
 import com.ecommers.models.Purchases;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface PurchasesRepository extends JpaRepository<Purchases, Long> {
 
+    List<Purchases> findByTotalPriceLessThanEqualOrderByTotalPrice(Double totalPriceIsLessThan);
+    List<Purchases> findByProduct_Id(Long productId);
+    List<Purchases> findByFinishedDateBetween(LocalDateTime finishedDateAfter, LocalDateTime finishedDateBefore);
 }
