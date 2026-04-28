a0340bb2a4a7d15a199d445c7b5255f44dfaa197
Alba Carrasco <albacarrasmel@gmail.com>
2026-04-28 15:13:33 +0200

    #16 Metodos creados PurchaseRepositoryTest

---
+++ src/test/java/com/ecommers/repository/PurchaseLineRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.ecommers.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PurchaseLineRepositoryTest {
+
+    @Test
+    void findByPurchase() {
+    }
+
+    @Test
+    void findByProduct() {
+    }
+
+    @Test
+    void findByQuatintyGreaterThan() {
+    }
+
+    @Test
+    void findByQuatintyLessThan() {
+    }
+
+    @Test
+    void findByQuatintyBetween() {
+    }
+
+    @Test
+    void findByPurchaseOrderByQuatintyDesc() {
+    }
+
+    @Test
+    void findByPurchaseAndProduct() {
+    }
+}
