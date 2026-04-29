f1059f6eede95de9d390c717639e843728f46911
Alba Carrasco <albacarrasmel@gmail.com>
2026-04-29 18:59:12 +0200

    #9 Added @ToString and @Builder.default to creationDate and PurchaseStatus, commented @PrePersist and @PreUpdate

--- src/main/java/com/ecommerce/model/Purchase.java
+++ src/main/java/com/ecommerce/model/Purchase.java
@@ -15,19 +15,23 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ToString
 public class Purchase {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id; // ID del producto comprado
 
+    @Builder.Default
     @Column(nullable = false)
-    private LocalDateTime creationDate; // Fecha y hora de inicio de la compra
+    private LocalDateTime creationDate = LocalDateTime.now(); // Fecha y hora de inicio de la compra
+
 
     private LocalDateTime finishedDate; // Fecha y hora de finalización de la compra
 
+    @Builder.Default
     @Enumerated(EnumType.STRING) // Estado de la compra: INICIADO, INACTIVO, TERMINADO
-    private PurchaseStatus purchaseStatus;
+    private PurchaseStatus purchaseStatus = PurchaseStatus.INICIADO;
 
     @Enumerated(EnumType.STRING) // Tipo de envío: STANDARD, EXPRESS, PREMIUM
     private ShippingMode shippingMode;
@@ -36,16 +40,16 @@ public class Purchase {
 
     private String userComment; // Requisitos especificados por el comprador a la hora de la entrega
 
-    @PrePersist // Esto garantiza valores por defecto al crear el registro de la compra
-    public void prePersist()
-    {
-        this.creationDate = LocalDateTime.now();
-    }
-
-    @PreUpdate //Para llevar un registro de la ultima modificación.
-    public void preUpdate() {
-        if(purchaseStatus == PurchaseStatus.TERMINADO){
-            this.finishedDate = LocalDateTime.now(); //Actualiza la fecha de finalización si el estado es TERMINADO
-        }
-    }
+//    @PrePersist // Esto garantiza valores por defecto al crear el registro de la compra
+//    public void prePersist()
+//    {
+//        this.creationDate = LocalDateTime.now();
+//    }
+//
+//    @PreUpdate //Para llevar un registro de la ultima modificación.
+//    public void preUpdate() {
+//        if(purchaseStatus == PurchaseStatus.TERMINADO){
+//            this.finishedDate = LocalDateTime.now(); //Actualiza la fecha de finalización si el estado es TERMINADO
+//        }
+//    }
 }
