bf7af159d10f5cba1273e5e97d7472103f3f7e5d
Alba Carrasco <albacarrasmel@gmail.com>
2026-05-04 00:57:37 +0200

    #18 added the elementary things to PurchaseController & PurchaseLineController

--- src/main/java/com/ecommerce/controller/PurchaseController.java
+++ src/main/java/com/ecommerce/controller/PurchaseController.java
@@ -1,8 +1,28 @@
 package com.ecommerce.controller;
 
+import com.ecommerce.repository.PurchaseRepository;
+import com.ecommerce.repository.ReviewRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
+@AllArgsConstructor
 public class PurchaseController {
 
+    private final PurchaseRepository purchaseRepository;
+
+    @GetMapping("purchases")
+    public String purchases(Model model) {
+        model.addAttribute("purchases", purchaseRepository.findAll());
+        return "purchases/purchase-detail";
+    }
+
+    @GetMapping("orders/{id}")
+    public String purchase(Model model, @PathVariable Long id) {
+        model.addAttribute("order", purchaseRepository.findById(id).orElseThrow());
+        return "purchases/purchase-detail";
+    }
 }
--- src/main/java/com/ecommerce/controller/PurchaseLineController.java
+++ src/main/java/com/ecommerce/controller/PurchaseLineController.java
@@ -1,8 +1,23 @@
 package com.ecommerce.controller;
 
+import com.ecommerce.repository.PurchaseLineRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PurchaseLineController {
+
+    private final PurchaseLineRepository purchaseLineRepository;
+
+    public PurchaseLineController(PurchaseLineRepository purchaseLineRepository) {
+        this.purchaseLineRepository = purchaseLineRepository;
+    }
+
+    @GetMapping("purchase_line")
+    public String purchases(Model model) {
+        model.addAttribute("purchase_line", purchaseLineRepository.findAll());
+        return "purchase_line/purchase_line-detail";
+    }
 
 }
