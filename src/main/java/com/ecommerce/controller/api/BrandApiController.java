662015b3b0ea853e3c4b85f205e209856f4d6de9
Kent Carrero <kent.iluminati@gmail.com>
2026-05-04 18:10:25 +0200

    #8 feat(api): add/update Brand API controller

--- src/main/java/com/ecommerce/controller/api/BrandApiController.java
+++ src/main/java/com/ecommerce/controller/api/BrandApiController.java
@@ -1,4 +1,48 @@
 package com.ecommerce.controller.api;
 
-public class BrandApiController {
+import com.ecommerce.dto.BrandRequestDto;
+import com.ecommerce.dto.BrandResponseDto;
+import com.ecommerce.service.BrandService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/brands")
+@RequiredArgsConstructor
+public class BrandApiController
+{
+    private final BrandService brandService;
+
+    @GetMapping
+    public List<BrandResponseDto> findAll()
+    {
+        return brandService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public BrandResponseDto findById(@PathVariable UUID id)
+    {
+        return brandService.findById(id);
+    }
+
+    @PostMapping
+    public BrandResponseDto create(@RequestBody BrandRequestDto dto)
+    {
+        return brandService.create(dto);
+    }
+
+    @PutMapping("/{id}")
+    public BrandResponseDto update(@PathVariable UUID id, @RequestBody BrandRequestDto dto)
+    {
+        return brandService.update(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable UUID id)
+    {
+        brandService.delete(id);
+    }
 }
