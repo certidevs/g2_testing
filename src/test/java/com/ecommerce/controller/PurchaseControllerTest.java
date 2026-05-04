03d86de5c6f990e5d0b2ddd815c06baed9e5c6bd
Alba Carrasco <albacarrasmel@gmail.com>
2026-05-04 12:29:07 +0200

    #19 ended the basic methods for PurchaseControllerTest & PurchaseLineControllerTest

--- src/test/java/com/ecommerce/controller/PurchaseControllerTest.java
+++ src/test/java/com/ecommerce/controller/PurchaseControllerTest.java
@@ -1,7 +1,10 @@
 package com.ecommerce.controller;
 
+import com.ecommerce.model.Product;
+import com.ecommerce.model.Purchase;
+import com.ecommerce.model.Users;
+import com.ecommerce.model.enums.*;
 import com.ecommerce.repository.ProductRepository;
-import com.ecommerce.repository.PurchaseLineRepository;
 import com.ecommerce.repository.PurchaseRepository;
 import com.ecommerce.repository.UsersRepository;
 import jakarta.transaction.Transactional;
@@ -12,7 +15,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.List;
+import java.util.UUID;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -23,9 +36,6 @@ class PurchaseControllerTest {
     PurchaseRepository purchaseRepository;
 
     @Autowired
-    PurchaseLineRepository purchaseLineRepository;
-
-    @Autowired
     ProductRepository productRepository;
 
     @Autowired
@@ -34,16 +44,156 @@ class PurchaseControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    Users user1;
+    Users user2;
+
+    Product product1;
+    Product product2;
+
+    Purchase purchase1;
+    Purchase purchase2;
+    Purchase purchase3;
+    Purchase purchase4;
+
     @BeforeEach
     void setUp(){
 
+        user1 = Users.builder()
+                .name("User 1")
+                .lastName("Last Name 1")
+                .email("user1@gmail.com")
+                .phone("123456789")
+                .password("password1")
+                .birthday(LocalDateTime.of(1990, Month.JANUARY, 1, 0, 0))
+                .gender(Gender.MALE)
+                .role(Role.CUSTOMER)
+                .build();
+
+        user2 = Users.builder()
+                .name("User 2")
+                .lastName("Last Name 2")
+                .email("user2@gmail.com")
+                .phone("987654321")
+                .password("password2")
+                .birthday(LocalDateTime.of(1995, Month.JUNE, 15, 0, 0))
+                .gender(Gender.FEMALE)
+                .role(Role.CUSTOMER)
+                .build();
+
+        usersRepository.saveAll(List.of(user1, user2));
+
+        product1 = Product.builder()
+                .title("Product 1")
+                .available(true)
+                .price(20.00)
+                .purchase(purchase1)
+                .purchase(purchase3)
+                .build();
+
+        product2 = Product.builder()
+                .title("Product 2")
+                .available(true)
+                .price(10.00)
+                .purchase(purchase2)
+                .purchase(purchase4)
+                .build();
+
+        productRepository.saveAll(List.of(product1, product2));
+
+        purchase1 = Purchase.builder()
+                .users(user1)
+                .creationDate(LocalDateTime.of(2026, Month.MARCH, 15, 12, 45))
+                .finishedDate(LocalDateTime.of(2026, Month.APRIL, 28, 17, 30))
+                .purchaseStatus(PurchaseStatus.FINISHED)
+                .paymentStatus(PaymentStatus.PAID)
+                .processStatus(ProcessStatus.COMPLETED)
+                .shippingStatus(ShippingStatus.DELIVERED)
+                .shippingMode(ShippingMode.STANDARD)
+                .totalPrice(50.00)
+                .userComment("Me ha llegado el producto en mal estado")
+                .build();
+
+        purchase2 = Purchase.builder()
+                .users(user2)
+                .creationDate(LocalDateTime.of(2025, Month.JUNE, 10, 18, 35))
+                .finishedDate(LocalDateTime.of(2025, Month.DECEMBER, 25, 16, 15))
+                .purchaseStatus(PurchaseStatus.FINISHED)
+                .paymentStatus(PaymentStatus.PAID)
+                .processStatus(ProcessStatus.COMPLETED)
+                .shippingStatus(ShippingStatus.DELIVERED)
+                .shippingMode(ShippingMode.EXPRESS)
+                .totalPrice(15.45)
+                .userComment("El producto ha llegado bien pero he tardado mucho más de lo esperado teniendo en cuenta que era EXPRESS")
+                .build();
+
+        purchase3 = Purchase.builder()
+                .users(user1)
+                .creationDate(LocalDateTime.of(2026, Month.FEBRUARY, 10, 11, 50))
+                .finishedDate(null)
+                .purchaseStatus(PurchaseStatus.INITIATED)
+                .paymentStatus(PaymentStatus.PENDING)
+                .processStatus(ProcessStatus.PENDING)
+                .shippingStatus(ShippingStatus.PENDING)
+                .shippingMode(ShippingMode.PREMIUM)
+                .totalPrice(150.75)
+                .userComment(null)
+                .build();
+
+        purchase4 = Purchase.builder()
+                .users(user2)
+                .creationDate(LocalDateTime.of(2020, Month.MAY, 30, 8, 30))
+                .finishedDate(null)
+                .purchaseStatus(PurchaseStatus.INACTIVE)
+                .paymentStatus(PaymentStatus.PENDING)
+                .processStatus(ProcessStatus.PENDING)
+                .shippingStatus(ShippingStatus.PENDING)
+                .shippingMode(ShippingMode.STANDARD)
+                .totalPrice(73.00)
+                .userComment(null)
+                .build();
+
+        purchaseRepository.saveAll(List.of(purchase1, purchase2, purchase3, purchase4));
     }
 
     @Test
-    void purchases() {
+    void purchasesFull() throws Exception {
+        mockMvc.perform(get("/purchases"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("purchases/purchases-list"))
+                .andExpect(model().attributeExists("purchases"))
+                .andExpect(model().attribute("purchases", hasSize(4)));
     }
 
     @Test
-    void purchase() {
+    void purchasesEmpty() throws Exception {
+        purchaseRepository.deleteAll();
+
+        mockMvc.perform(get("/purchases"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("purchases/purchases-list"))
+                .andExpect(model().attributeExists("purchases"))
+                .andExpect(model().attribute("purchases", hasSize(0)));
+    }
+
+    @Test
+    void purchaseDetailIsPresentTrue() throws Exception {
+
+        Purchase purchase = new Purchase();
+        purchase = purchaseRepository.save(purchase);
+
+        mockMvc.perform(get("/purchases/{id}", purchase.getId()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("purchases/purchase-detail"))
+                .andExpect(model().attributeExists("purchase"))
+                .andExpect(model().attribute("purchase", hasProperty("id", is(purchase.getId()))));
+    }
+
+    @Test
+    void purchaseDetailIsPresentFalse() throws Exception {
+
+        UUID randomId = UUID.randomUUID();
+
+        mockMvc.perform(get("/purchases/{id}", randomId))
+                .andExpect(status().isNotFound());
     }
 }
--- src/test/java/com/ecommerce/controller/PurchaseLineControllerTest.java
+++ src/test/java/com/ecommerce/controller/PurchaseLineControllerTest.java
@@ -1,5 +1,9 @@
 package com.ecommerce.controller;
 
+import com.ecommerce.model.Product;
+import com.ecommerce.model.Purchase;
+import com.ecommerce.model.PurchaseLine;
+import com.ecommerce.model.enums.*;
 import com.ecommerce.repository.ProductRepository;
 import com.ecommerce.repository.PurchaseLineRepository;
 import com.ecommerce.repository.PurchaseRepository;
@@ -11,7 +15,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.UUID;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -30,16 +43,106 @@ class PurchaseLineControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    Purchase purchase1;
+    Purchase purchase2;
+    Purchase purchase3;
+    Purchase purchase4;
+    PurchaseLine purchaseLine1;
+    PurchaseLine purchaseLine2;
+    PurchaseLine purchaseLine3;
+    PurchaseLine purchaseLine4;
+    Product product1;
+    Product product2;
+    Product product3;
+    Product product4;
+
     @BeforeEach
     void setUp(){
 
+        product1 = Product.builder().title("pr1").price(20.50).build();
+        product2 = Product.builder().title("pr2").price(30.00).build();
+        product3 = Product.builder().title("pr3").price(10.00).build();
+        product4 = Product.builder().title("pr4").price(5.00).build();
+        productRepository.saveAll(List.of(product1, product2, product3, product4));
+
+        purchase1 = Purchase.builder().totalPrice(20.50).build();
+        purchase2 = Purchase.builder().totalPrice(60.00).build();
+        purchase3 = Purchase.builder().totalPrice(40.00).build();
+        purchase4 = Purchase.builder().totalPrice(50.00).build();
+        purchaseRepository.saveAll(List.of(purchase1, purchase2, purchase3, purchase4));
+
+        purchaseLine1 = PurchaseLine.builder()
+                .quantity(1)
+                .product(product1)
+                .purchase(purchase1)
+                .build();
+        purchaseLine2 = PurchaseLine.builder()
+                .quantity(2)
+                .product(product1)
+                .purchase(purchase2)
+                .build();
+        purchaseLine3 = PurchaseLine.builder()
+                .quantity(4)
+                .product(product3)
+                .purchase(purchase3)
+                .build();
+        purchaseLine4 = PurchaseLine.builder()
+                .quantity(10)
+                .product(product4)
+                .purchase(purchase1)
+                .build();
+        purchaseLineRepository.saveAll(List.of(purchaseLine1, purchaseLine2, purchaseLine3, purchaseLine4));
     }
 
     @Test
-    void purchases() {
+    void purchaseLinesFull() throws Exception {
+
+        mockMvc.perform(get("/purchase_lines"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("purchase_lines/purchase_line-list"))
+                .andExpect(model().attributeExists("purchaseLines"));
     }
 
     @Test
-    void purchase() {
+    void purchaseLinesEmpty() throws Exception {
+
+        purchaseLineRepository.deleteAll();
+
+        mockMvc.perform(get("/purchase_lines"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("purchase_lines/purchase_line-list"))
+                .andExpect(model().attributeExists("purchaseLines"))
+                .andExpect(model().attribute("purchaseLines", hasSize(0)));
+    }
+
+    @Test
+    void purchaseLineIsPresentTrue() throws Exception {
+
+        PurchaseLine purchaseLine = PurchaseLine.builder()
+                .quantity(3)
+                .product(product2)
+                .purchase(purchase4)
+                .build();
+        purchaseLine = purchaseLineRepository.save(purchaseLine);
+
+        mockMvc.perform(get("/purchase_lines"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("purchase_lines/purchase_line-list"))
+                .andExpect(model().attributeExists("purchaseLines"))
+                .andExpect(model().attribute("purchaseLines", hasItem(
+                        hasProperty("id", is(purchaseLine.getId()))
+                )));
+    }
+
+    @Test
+    void purchaseLineIsPresentFalse() throws Exception {
+
+        UUID randomId = UUID.randomUUID();
+
+        mockMvc.perform(get("/purchase_lines"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("purchaseLines", not(hasItem(
+                        hasProperty("id", is(randomId))
+                ))));
     }
 }
