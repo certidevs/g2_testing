093bcbfe92dbe6684fddf859349946f039230d9d
Alba Carrasco <albacarrasmel@gmail.com>
2026-05-05 16:26:14 +0200

    changed the html of purchases-list.html & purchases-detail.html

--- src/main/java/com/ecommerce/controller/PurchaseController.java
+++ src/main/java/com/ecommerce/controller/PurchaseController.java
@@ -1,12 +1,16 @@
 package com.ecommerce.controller;
 
+import com.ecommerce.repository.PurchaseLineRepository;
 import com.ecommerce.repository.PurchaseRepository;
+import com.ecommerce.service.PurchaseService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.UUID;
 
@@ -15,6 +19,7 @@ import java.util.UUID;
 public class PurchaseController {
 
     private final PurchaseRepository purchaseRepository;
+    private final PurchaseLineRepository purchaseLineRepository;
 
     @GetMapping("purchases")
     public String listPurchases(Model model) {
@@ -25,6 +30,14 @@ public class PurchaseController {
     @GetMapping("purchases/{id}")
     public String detailPurchase(Model model, @PathVariable UUID id) {
         model.addAttribute("purchase", purchaseRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)));
+        model.addAttribute("purchaseLines", purchaseLineRepository.findByPurchase(purchaseRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND))));
         return "purchases/purchase-detail";
+    }
+
+    @GetMapping("purchases/delete/{id}")
+    public String deletePurchase(@PathVariable UUID id, RedirectAttributes redirectAttributes) {
+        purchaseRepository.deleteById(id);
+        redirectAttributes.addFlashAttribute("message", "Purchase deleted successfully");
+        return "redirect:/purchases";
     }
 }
