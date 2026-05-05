2ed07c1c527442ede8c12ae986b386b188e03174
jeanpool <jeanpool_0612@hotmail.com>
2026-05-05 16:33:16 +0200

    agrego en product lo que es una descripcion

--- src/main/java/com/ecommerce/config/DataInitializer.java
+++ src/main/java/com/ecommerce/config/DataInitializer.java
@@ -27,6 +27,7 @@ public class DataInitializer  implements CommandLineRunner {
         var brand1 = brandRepo.save(Brand.builder().name("Nike").nif("123456789").build());
 
         var product1 = productRepo.save(Product.builder().title("Camiseta Blanca")
+                        .longDescription("Esta camiseta blanca de Nike es perfecta para cualquier ocasión. Confeccionada con algodón de alta calidad.")
                 .imageUrl("https://static.nike.com/a/images/t_web_pw_592_v2/f_auto/0b73e9b3-1bba-4a6b-8d4b-ecc9d2a473c8/M+NSW+TEE+M90+FW+MBR+CNCT+HO25.png")
                 .price(30.00).brand(brand1).build());
         var product2 = productRepo.save(Product.builder().title("Pantalon Deporte").imageUrl("https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcR6ErZKh_-lTTwptKNoNRmIE7wEd-IIm_GQsU3c5SjQxDU2kj0678uu_vFbVX2QgNrLozE4rcSjki-WGpfYw83KzWSmPjNus4dd5V4hNJJb-SSn-lCSMw5Ez6kRMYbLGeh3rUoMWCpx&usqp=CAc")
