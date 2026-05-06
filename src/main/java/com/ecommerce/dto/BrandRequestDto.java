5e414451cd33e94d26004392c563c7cded8f7d5a
Kent Carrero <kent.iluminati@gmail.com>
2026-05-06 21:19:27 +0200

    #8 dto: actualizar BrandRequestDto (validaciones y campos)

--- src/main/java/com/ecommerce/dto/BrandRequestDto.java
+++ src/main/java/com/ecommerce/dto/BrandRequestDto.java
@@ -1,5 +1,7 @@
 package com.ecommerce.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -7,13 +9,25 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-// Lo que el cliente manda para crear/editar un producto.
+// Lo que el cliente manda para crear/editar una marca.
 public class BrandRequestDto
 {
+    @NotBlank(message = "El nombre de la marca es obligatorio")
+    @Size(max = 100, message = "El nombre no puede superar 100 caracteres")
     private String name;
+
+    @NotBlank(message = "El NIF es obligatorio")
+    @Size(min = 9, max = 9, message = "El NIF debe tener 9 caracteres")
     private String nif;
+
+    @Size(max = 100, message = "EL país no puede superar 100 caracteres")
     private String country;
+
+    @Size(max = 255, message = "La web no puede superar los 255 caracteres")
     private String website;
+
+    @Size(max = 255, message = "El logo no puede superar los 255 caracteres")
     private String logo;
+
     private Boolean active;
 }
