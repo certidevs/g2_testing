006e2602c8fc52f1ed83de9616b387ad7763119e
Kent Carrero <kent.iluminati@gmail.com>
2026-05-13 16:25:43 +0200

    #8 creacion vacia de los test para los dtos de brands

---
+++ src/test/java/com/ecommerce/dto/BrandRequestDtoTest.java
@@ -0,0 +1,65 @@
+package com.ecommerce.dto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BrandRequestDtoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getNif() {
+    }
+
+    @Test
+    void getCountry() {
+    }
+
+    @Test
+    void getWebsite() {
+    }
+
+    @Test
+    void getLogo() {
+    }
+
+    @Test
+    void getActive() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setNif() {
+    }
+
+    @Test
+    void setCountry() {
+    }
+
+    @Test
+    void setWebsite() {
+    }
+
+    @Test
+    void setLogo() {
+    }
+
+    @Test
+    void setActive() {
+    }
+
+    @Test
+    void builder() {
+    }
+}
