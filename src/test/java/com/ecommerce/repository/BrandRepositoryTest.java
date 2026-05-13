2920bb56e48a4755c70eef1020f1dbdaa72f8b46
Kent Carrero <kent.iluminati@gmail.com>
2026-05-13 16:28:45 +0200

    #8 creacion de los test vacios para el repository de brand

---
+++ src/test/java/com/ecommerce/repository/BrandRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.ecommerce.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BrandRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByName() {
+    }
+
+    @Test
+    void existsByName() {
+    }
+
+    @Test
+    void findByNif() {
+    }
+
+    @Test
+    void existsByNif() {
+    }
+
+    @Test
+    void existsByNameAndIdNot() {
+    }
+
+    @Test
+    void existsByNifAndIdNot() {
+    }
+}
