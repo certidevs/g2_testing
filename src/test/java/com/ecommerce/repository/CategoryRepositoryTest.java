c823190aae0a6f73079550724879499458acdf34
Kent Carrero <kent.iluminati@gmail.com>
2026-05-13 16:29:59 +0200

    #7 creacion de los test vacios del repository de category

---
+++ src/test/java/com/ecommerce/repository/CategoryRepositoryTest.java
@@ -0,0 +1,53 @@
+package com.ecommerce.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findBySlug() {
+    }
+
+    @Test
+    void existsBySlug() {
+    }
+
+    @Test
+    void existsByName() {
+    }
+
+    @Test
+    void findByParentIsNull() {
+    }
+
+    @Test
+    void findByParentId() {
+    }
+
+    @Test
+    void existsByParentIdAndSlug() {
+    }
+
+    @Test
+    void existsByParentIsNullAndSlug() {
+    }
+
+    @Test
+    void findAllRootWithChildren() {
+    }
+
+    @Test
+    void findAllRootWithChildrenEntityGraph() {
+    }
+
+    @Test
+    void findByIdWithCategories() {
+    }
+}
