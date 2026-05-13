a9f62b2ca8457fe3d161617a0092238d14b3c443
Kent Carrero <kent.iluminati@gmail.com>
2026-05-13 16:16:55 +0200

    #7 creacion de los test vacios del controlador de categorias

---
+++ src/test/java/com/ecommerce/controller/web/CategoryControllerTest.java
@@ -0,0 +1,45 @@
+package com.ecommerce.controller.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findTree() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void showCreateForm() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void showEditForm() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
