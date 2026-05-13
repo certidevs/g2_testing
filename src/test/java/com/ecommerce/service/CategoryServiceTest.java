a4d044c2702e2db9131e9a33880adcab45a949c2
Kent Carrero <kent.iluminati@gmail.com>
2026-05-13 16:32:14 +0200

    #7 creacion de los test vacios del service de category

---
+++ src/test/java/com/ecommerce/service/CategoryServiceTest.java
@@ -0,0 +1,41 @@
+package com.ecommerce.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findRootCategories() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getCategoryAndChildrenIds() {
+    }
+}
