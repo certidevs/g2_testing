c4cd2f2f605a0b9d47ec75a4b2bc6de15bf89de9
Kent Carrero <kent.iluminati@gmail.com>
2026-05-13 16:17:24 +0200

    #8 creacion de los test vacios del controlador de brand

--- src/test/java/com/ecommerce/controller/web/BrandControllerTest.java
+++ src/test/java/com/ecommerce/controller/web/BrandControllerTest.java
@@ -1,4 +1,41 @@
+package com.ecommerce.controller.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class BrandControllerTest {
-  
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void showCreateForm() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void showEditForm() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
 }
