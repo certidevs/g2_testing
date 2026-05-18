bea0824a823d8c4930202017dbde0c076589486c
Alba Carrasco <albacarrasmel@gmail.com>
2026-05-18 15:23:51 +0200

    added the base of PurchaseLineServiceTest and PurchaseServiceTest

---
+++ src/test/java/com/ecommerce/service/PurchaseLineServiceTest.java
@@ -0,0 +1,36 @@
+package com.ecommerce.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PurchaseLineServiceTest {
+
+    @Test
+    void getAllPurchaseLines() {
+    }
+
+    @Test
+    void getPurchaseLineById() {
+    }
+
+    @Test
+    void createPurchaseLine() {
+    }
+
+    @Test
+    void updatePurchaseLine() {
+    }
+
+    @Test
+    void updateQuantity() {
+    }
+
+    @Test
+    void deletePurchaseLine() {
+    }
+
+    @Test
+    void getPurchaseLinesByPurchase() {
+    }
+}
---
+++ src/test/java/com/ecommerce/service/PurchaseServiceTest.java
@@ -0,0 +1,60 @@
+package com.ecommerce.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PurchaseServiceTest {
+
+    @Test
+    void createPurchase() {
+    }
+
+    @Test
+    void getAllPurchases() {
+    }
+
+    @Test
+    void getPurchaseById() {
+    }
+
+    @Test
+    void updatePurchase() {
+    }
+
+    @Test
+    void deletePurchase() {
+    }
+
+    @Test
+    void getPurchasesByUserId() {
+    }
+
+    @Test
+    void updatePurchaseStatus() {
+    }
+
+    @Test
+    void updatePaymentStatus() {
+    }
+
+    @Test
+    void updateProcessStatus() {
+    }
+
+    @Test
+    void updateShippingStatus() {
+    }
+
+    @Test
+    void updateShippingMode() {
+    }
+
+    @Test
+    void updateUserComment() {
+    }
+
+    @Test
+    void finishPurchase() {
+    }
+}
