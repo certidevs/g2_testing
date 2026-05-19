ac0b1cad88547be5546a90703c867230e9e93213
Alba Carrasco <albacarrasmel@gmail.com>
2026-05-19 23:21:04 +0200

    #14 Cambio de comentarios de inglés a español PurchaseRepository y agregación de nuevas funciones para buscar carritos de compras iniciados

--- src/main/java/com/ecommerce/repository/PurchaseRepository.java
+++ src/main/java/com/ecommerce/repository/PurchaseRepository.java
@@ -16,79 +16,82 @@ public interface PurchaseRepository extends JpaRepository<Purchase, UUID> {
 
     // -------- SIMPLES --------
 
-    // Specific user
+    // Usuario específico
     List<Purchase> findByUserId(UUID users_id);
 
-    // Purchase status
+    // Estado de compra
     List<Purchase> findByPurchaseStatus(PurchaseStatus purchaseStatus);
 
-    // Payment status
+    // Estado de pago
     List<Purchase> findByPaymentStatus(PaymentStatus paymentStatus);
 
-    // Process status
+    // Estado de proceso
     List<Purchase> findByProcessStatus(ProcessStatus processStatus);
 
-    // Shipping status
+    // Estado de envío
     List<Purchase> findByShippingStatus(ShippingStatus shippingStatus);
 
-    // Shipping mode
+    // Modo de envío
     List<Purchase> findByShippingMode(ShippingMode shippingMode);
 
-    // --------- RANGES --------
+    // --------- RANGOS --------
 
+    // Compras creadas entre dos fechas
     List<Purchase> findByCreationDateBetween(LocalDateTime creationDateAfter, LocalDateTime creationDateBefore);
 
-    // Finished data between
+    // Compras finalizadas entre dos fechas
     List<Purchase> findByFinishedDateBetween(LocalDateTime finishedDateAfter, LocalDateTime finishedDateBefore);
 
-    // Total price between
+    // Compras con precio total entre dos valores
     List<Purchase> findByTotalPriceBetween(Double minTotalPrice, Double maxTotalPrice);
 
-    // Total price greater than a value
+    // Compras con precio total mayor a un valor
     List<Purchase> findByTotalPriceGreaterThan(Double totalPrice);
 
-    // Total price less than a value
+    // Compras con precio total menor a un valor
     List<Purchase> findByTotalPriceLessThan(Double totalPrice);
 
-    // -------- SPECIFICS --------
+    // -------- ESPECÍFICOS --------
 
-    // Specific purchase & user comment (key word)
+    // Compras de un producto específico y comentario del usuario (key word)
     List<Purchase> findByIdAndUserCommentContaining(UUID product_id, String userComment);
 
-    // Specific user & user comment (key word)
+    // Usuario específico y comentario del usuario (key word)
     List<Purchase> findByUserIdAndUserCommentContaining(UUID user_id, String userComment);
 
-    // User comment (key word)
+    // Comentario del usuario (key word)
     List<Purchase> findByUserCommentContaining(String keyword);
 
-    // -------- ORDER --------
+    // -------- ORDEN --------
 
-    // Sort by creation date descendent
+    // Ordenar por fecha de creación descendente
     List<Purchase> findAllByOrderByCreationDateDesc();
 
-    // Sort by creation date ascendant
+    // Ordenar por fecha de creación ascendente
     List<Purchase> findAllByOrderByCreationDateAsc();
 
-    // -------- COMPLEX --------
+    // -------- COMPLEJOS --------
 
-    // Purchases finished on range date and sort by total price
+    // Compras terminadas entre dos fechas y con un estado de compra específico, ordenadas por precio total
     List<Purchase> findByFinishedDateBetweenAndPurchaseStatusOrderByTotalPrice(
             LocalDateTime start, LocalDateTime end, PurchaseStatus status);
 
-     // Specific purchase & shipping mode
+    // Compra especifica con modo de envío específico
     List<Purchase> findByIdAndShippingMode(UUID id, ShippingMode shippingMode);
 
-    // -------- METHODS --------
+    // -------- FUNCTIONS --------
 
-    // Check if user has purchased a specific product through PurchaseLine
+    // Comprueba si existe una compra de un producto específico para un usuario específico, devuelve true si existe, false si no existe
     @Query("SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END " +
-           "FROM Purchase p " +
-           "JOIN p.lines pl " +
-           "JOIN pl.product pr " +
-           "WHERE p.user.id = :userId AND pr.id = :productId")
+            "FROM Purchase p " +
+            "JOIN p.lines pl " +
+            "JOIN pl.product pr " +
+            "WHERE p.user.id = :userId AND pr.id = :productId")
     boolean existsByUsersIdAndProductId(@Param("userId") UUID userId, @Param("productId") UUID productId);
 
+    // Busca el primer carrito iniciado, sin importar el usuario, para mostrarlo en el carrito de compras, si no hay ningún carrito iniciado, devuelve un Optional vacío
     Optional<Purchase> findFirstByPurchaseStatus(PurchaseStatus purchaseStatus);
 
-    //List<Purchase> findByUserIdAndUserCommentContaining(UUID userId, String userComment);
+    // Busca el primer carrito iniciado que pertenezca estrictamente al usuario actual
+    Optional<Purchase> findFirstByUserIdAndPurchaseStatus(UUID userId, PurchaseStatus purchaseStatus);
 }
