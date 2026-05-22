5755910ace7894c84c4bfd152c3ff7bc577b2e76
Alba Carrasco <albacarrasmel@gmail.com>
2026-05-22 19:42:52 +0200

    He agregado con la anotacion @Mock los servicios de purchaseService y purchaselineService

--- src/test/java/com/ecommerce/service/PurchaseLineServiceTest.java
+++ src/test/java/com/ecommerce/service/PurchaseLineServiceTest.java
@@ -1,13 +1,22 @@
 package com.ecommerce.service;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PurchaseLineServiceTest {
 
+
+    @Mock
+    private PurchaseLineService purchaseLineService;
+
+    @Mock
+    private PurchaseService purchaseService;
+
     @Test
     void getAllPurchaseLines() {
+
     }
 
     @Test
