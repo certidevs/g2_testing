fe8fde75a584ec55c30e73722f227ba23bfb8332
Alba Carrasco <albacarrasmel@gmail.com>
2026-06-02 15:15:00 +0200

    agregada la base de los test de selenium (todavía quedan cosas pendientes en la base)

--- src/test/java/com/ecommerce/ui/BaseSeleniumTest.java
+++ src/test/java/com/ecommerce/ui/BaseSeleniumTest.java
@@ -4,8 +4,7 @@ import com.ecommerce.model.Product;
 import com.ecommerce.model.Purchase;
 import com.ecommerce.model.Review;
 import com.ecommerce.model.User;
-import com.ecommerce.model.enums.AddressType;
-import com.ecommerce.model.enums.Role;
+import com.ecommerce.model.enums.*;
 import com.ecommerce.repository.ProductRepository;
 import com.ecommerce.repository.PurchaseRepository;
 import com.ecommerce.repository.ReviewRepository;
@@ -44,38 +43,36 @@ public class BaseSeleniumTest {
     WebDriver driver;
     WebDriverWait wait;
 
-    Restaurant pizzeria; // con platos
-    Restaurant taberna; // sin platos
-    Dish pizza;
-    Dish tiramisu;
-    Review pizzeriaOK;
-    Review pizzeriaMal;
-    Review pizzaOK;
+    Purchase compraConProductos;
+    Purchase compraSinProductos;
+    Product camiseta;
+    Product pantalon;
+    Review productMal;
+    Review productOK;
     User user, admin;
 
     @BeforeEach
     void setUp() {
         // crear datos demo
-        reviewRepo.deleteAll();
-        dishRepo.deleteAll();
-        restaurantRepo.deleteAll();
-        userRepo.deleteAll();
+        reviewRepository.deleteAll();
+        productRepository.deleteAll();
+        purchaseRepository.deleteAll();
+        userRepository.deleteAll();
 
-        user = userRepo.save(User.builder()
-                .username("user").email("user@gmail.com").password(passwordEncoder.encode("user")).role(Role.ROLE_USER)
+        user = userRepository.save(User.builder()
+                .username("user").email("user@gmail.com").password(passwordEncoder.encode("user")).role(Role.ROLE_CUSTOMER)
                 .build());
 
-        admin = userRepo.save(User.builder()
+        admin = userRepository.save(User.builder()
                 .username("admin").email("admin@gmail.com").password(passwordEncoder.encode("admin")).role(Role.ROLE_ADMIN)
                 .build());
 
-        pizzeria = restaurantRepo.save(Restaurant.builder().name("Pizzeria Luigi").averagePrice(10.0).active(true).description("Masa artesanal").build());
-        taberna = restaurantRepo.save(Restaurant.builder().name("Taberna").averagePrice(20.0).active(false).build());
-        pizza = dishRepo.save(Dish.builder().name("Pizza 4 Quesos").price(12d).type(DishType.MAIN_COURSE).description("pizza bien").restaurant(pizzeria).build());
-        tiramisu = dishRepo.save(Dish.builder().name("Tiramisú Café").price(3d).type(DishType.DESSERT).description("fetén").restaurant(pizzeria).build());
-        pizzeriaOK = reviewRepo.save(Review.builder().title("Pectacular").rating(5).restaurant(pizzeria).content("Asombroso").build());
-        pizzeriaMal = reviewRepo.save(Review.builder().title("Fatal").rating(1).restaurant(pizzeria).content("Nada bien").creationDate(LocalDateTime.now().minusDays(1)).build());
-        pizzaOK = reviewRepo.save(Review.builder().title("excelente pizza").rating(5).dish(pizza).content("ok").creationDate(LocalDateTime.now().minusDays(1)).build());
+        compraConProductos = purchaseRepository.save(Purchase.builder().creationDate(LocalDateTime.now()).user(user).purchaseStatus(PurchaseStatus.INITIATED).shippingMode(ShippingMode.STANDARD).shippingStatus(ShippingStatus.PENDING).paymentStatus(PaymentStatus.PENDING).processStatus(ProcessStatus.PROCESSING).totalPrice(100.0).build());;
+        compraSinProductos = purchaseRepository.save(Purchase.builder().creationDate(LocalDateTime.now()).user(user).purchaseStatus(PurchaseStatus.INITIATED).shippingMode(ShippingMode.STANDARD).shippingStatus(ShippingStatus.PENDING).paymentStatus(PaymentStatus.PENDING).processStatus(ProcessStatus.ON_HOLD).totalPrice(0.0).build());
+        camiseta = productRepository.save(Product.builder().title("Camiseta").shortDescription("Camiseta de algodón").isbn("1234567890").price(20.0).stock(100).build());
+        pantalon = productRepository.save(Product.builder().title("Pantalón").shortDescription("Pantalón vaquero").isbn("0987654321").price(40.0).stock(50).build());
+        productMal = reviewRepository.save(Review.builder().title("Fatal").rating(1).product(pantalon).message("Fatal").creationDate(LocalDateTime.now().minusDays(1)).build());
+        productOK = reviewRepository.save(Review.builder().title("excelente pizza").rating(5).product(camiseta).message("ok").creationDate(LocalDateTime.now().minusDays(1)).build());
 
         // inicializar y configuración de driver
         baseUrl = "http://localhost:" + port + "/";
@@ -91,7 +88,6 @@ public class BaseSeleniumTest {
             driver.quit();
         }
     }
-
 
     void loginAdmin() {
         login("admin", "admin");
