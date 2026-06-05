810500bb969359dd122ffc97e7f4d2a15ad8f914
Alba Carrasco <albacarrasmel@gmail.com>
2026-06-05 17:30:16 +0200

    agregada la función de edit product el cual lleva un TODO incorporado porque hay que poner el identificar "nameproduct" a la casilla de cambiar el nombre del producto ya que no la he localizado y agregada la función logout la cual no está finalizada

--- src/test/java/com/ecommerce/ui/SecuritySeleniumTest.java
+++ src/test/java/com/ecommerce/ui/SecuritySeleniumTest.java
@@ -2,7 +2,10 @@ package com.ecommerce.ui;
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SecuritySeleniumTest extends BaseSeleniumTest{
 
@@ -22,5 +25,22 @@ public class SecuritySeleniumTest extends BaseSeleniumTest{
         driver.get(baseUrl + "products/new");
         wait.until(driver -> driver.findElement(By.tagName("h2"))
                 .getText().contains("No tienes permisos para acceder a esta sección."));
+    }
+
+    // Verifica que un usuario con rol admin pueda acceder a la página de creación de productos
+    @Test
+    void editProduct(){
+        loginAdmin();
+        driver.get(baseUrl + "products/edit/" + camiseta.getId());
+
+        // Verifica que el valor del nombre del producto aparezca en la ranura de nombre para editar
+        WebElement nameInput = driver.findElement(By.id("nameProduct")); // TODO poner el id "nameProduct" a la casilla donde se edita el nombre del producto
+        assertEquals("Camiseta", nameInput.getAttribute("value"));
+    }
+
+    @Test
+    void logout(){
+        loginUser();
+        driver.findElement(By.id("logoutBtn")).click();
     }
 }
